4af592b1c4176a993c5909044617314bc5ab8e31
Melih Ensar BARIŞIK <barisik.melih@gmail.com>
2020-12-28 00:47:08 +0300

    baslangic

---
+++ webProjeCalismasi/webProjeCalismasi/Models/Kitaplar.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace webProjeCalismasi.Models
+{
+    public class Kitaplar
+    {
+        public string kitapIsmi { get; set; }
+    }
+}
